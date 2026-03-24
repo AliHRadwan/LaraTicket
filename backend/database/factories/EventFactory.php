f7fdc786fc2ece6698684e932f288ffa594165f2
AliHRadwan <alihradwan@outlook.com>
2026-03-24 20:03:35 +0200

    fix(backend): fixing EventFactory

--- backend/database/factories/EventFactory.php
+++ backend/database/factories/EventFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use App\Models\Event;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use Illuminate\Support\Str;
 /**
  * @extends Factory<Event>
  */
@@ -21,17 +21,18 @@ class EventFactory extends Factory
         $start_datetime = fake()->dateTime();
         $end_datetime = fake()->dateTimeBetween($start_datetime, '+1 hour');
         $total_tickets = fake()->numberBetween(100, 500);
-        $user = User::random();
+        $available_tickets = $total_tickets - fake()->numberBetween(0, $total_tickets);
+        $user = User::inRandomOrder()->first();
         if (!$user) {
             $user = User::factory()->create();
         }
         return [
             'title' => fake()->sentence(),
-            'slug' => fake()->slug(),
-            'description' => fake()->paragraph(),
-            'price' => fake()->randomFloat(2, 0, 5000),
+            'slug' => Str::slug(fake()->sentence()),
+            'description' => fake()->text(),
+            'price' => round(fake()->randomFloat(2, 0, 5000), 2),
             'total_tickets' => $total_tickets,
-            'available_tickets' => $total_tickets - fake()->numberBetween(0, $total_tickets),
+            'available_tickets' => $available_tickets,
             'start_datetime' => $start_datetime,
             'end_datetime' => $end_datetime,
             'location' => fake()->address(),
