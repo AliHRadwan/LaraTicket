f082f4f6cfa9ad527c43de6cd4ddf5d9b6b1eda0
AliHRadwan <alihradwan@outlook.com>
2026-03-24 21:32:13 +0200

    fix(backend): fixing the PaymentGatewayInterface

--- backend/app/Contracts/PaymentGatewayInterface.php
+++ backend/app/Contracts/PaymentGatewayInterface.php
@@ -6,6 +6,6 @@ use App\Models\Order;
 
 interface PaymentGatewayInterface
 {
-    public function processPayment(Order $order, array $metadata = []): string;
+    public function processPayment(Order $order, array $metadata = []): object;
     public function refundPayment(string $transactionId, int|null $amountInCents = null): bool;
 }
