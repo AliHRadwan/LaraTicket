87efdd22d48dbb3bdcdbf17b4173df5b666e4994
AliHRadwan <alihradwan@outlook.com>
2026-03-25 10:39:31 +0200

    fix(backend): fixing the stripe public key name mismatch with the .env file

--- backend/config/services.php
+++ backend/config/services.php
@@ -35,7 +35,7 @@ return [
         ],
     ],
     'stripe' => [
-        'public_key' => env('STRIPE_KEY'),
+        'public_key' => env('STRIPE_PUBLIC_KEY'),
         'secret_key' => env('STRIPE_SECRET_KEY'),
         'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
     ],
