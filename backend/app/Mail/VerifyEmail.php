2b283b03385b26269a416c20579823646dad301a
AliHRadwan <alihradwan@outlook.com>
2026-03-25 02:21:24 +0200

    feat(backend): creating VerifyEmail and its blade view

---
+++ backend/app/Mail/VerifyEmail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class VerifyEmail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(
+        public User $user,
+        public string $verificationUrl,
+    ) {}
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Verify Your Email Address',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'emails.verify-email',
+        );
+    }
+
+    public function attachments(): array
+    {
+        return [];
+    }
+}
