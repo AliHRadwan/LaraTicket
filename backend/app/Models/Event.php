44dcc01d8cee04533c49b927cab32cdaa8227b9c
AliHRadwan <alihradwan@outlook.com>
2026-03-25 02:28:40 +0200

    fix(backend): adding the $fillable properties to the Event model and defining the casts() method, and the relationships with other models

--- backend/app/Models/Event.php
+++ backend/app/Models/Event.php
@@ -4,9 +4,44 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
     /** @use HasFactory<\Database\Factories\EventFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'description',
+        'price',
+        'total_tickets',
+        'available_tickets',
+        'start_datetime',
+        'end_datetime',
+        'location',
+        'image_url',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'price' => 'decimal:2',
+            'start_datetime' => 'datetime',
+            'end_datetime' => 'datetime',
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
 }
